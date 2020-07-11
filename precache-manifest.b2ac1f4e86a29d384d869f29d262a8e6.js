70c6948518f6f1c523f259e843db7161c0f985e7
Ripudaman Singh <ripu502@gmail.com>
2020-07-12 00:31:48 +0530

    host

---
+++ precache-manifest.b2ac1f4e86a29d384d869f29d262a8e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add5c5aaf031204ded60350b401fdf36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d6523b0f1a701e7adfa",
+    "url": "/static/css/main.cc2fc2eb.chunk.css"
+  },
+  {
+    "revision": "097f7f1b0f80a10bf43d",
+    "url": "/static/js/2.288186ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.288186ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d6523b0f1a701e7adfa",
+    "url": "/static/js/main.bf04ef3e.chunk.js"
+  },
+  {
+    "revision": "92efe62ac771f3d04de5",
+    "url": "/static/js/runtime-main.ff4f2b8d.js"
+  }
+]);
